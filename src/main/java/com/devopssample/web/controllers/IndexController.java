8168e62ed46dd4a008f18b95a378b1ad5debcc89
Katherine Guzman <kceleste35@gmail.com>
2016-12-02 09:03:29 -0500

    new changes

--- src/main/java/com/devopssample/web/controllers/IndexController.java
+++ src/main/java/com/devopssample/web/controllers/IndexController.java
@@ -1,7 +1,17 @@
 package com.devopssample.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by katherine_celeste on 12/2/16.
  */
+
+@Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
 }
